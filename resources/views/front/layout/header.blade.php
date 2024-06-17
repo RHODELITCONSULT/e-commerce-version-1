bc9ec2cc6c2964a0fb7d6781a204a49c6c53c969
OS <os@OSs-MacBook-Pro.local>
2024-06-17 17:18:01 +0000

    frontend header url fix

--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Models\Category;
 // Get Categories and their Sub Categories
 $categories = Category::getCategories();
@@ -6,81 +7,81 @@ $categories = Category::getCategories();
 $totalCartItems = totalCartItems();
 ?>
 <header class="header--style-1">
-<!--====== Nav 1 ======-->
-<nav class="primary-nav primary-nav-wrapper--border">
-    <div class="container">
-        <!--====== Primary Nav ======-->
-        <div class="primary-nav">
-            <!--====== Main Logo ======-->
-            <a class="main-logo" href="{{ url('/')}}">
-            <img src="{{ asset('front/images/logo/logo-1.png') }}" alt=""></a>
-            <!--====== End - Main Logo ======-->
-            <!--====== Search Form ======-->
-            <form class="main-form" action="{{ url('search-products') }}" method="get">
-                <label for="main-search"></label>
-                <input class="input-text input-text--border-radius input-text--style-1" type="text" id="main-search" name="query" placeholder="Search">
-                <button class="btn btn--icon fas fa-search main-search-button" type="submit"></button>
-            </form>
-            <!--====== End - Search Form ======-->
-            <!--====== Dropdown Main plugin ======-->
-            <div class="menu-init" id="navigation">
-                <button class="btn btn--icon toggle-button toggle-button--secondary fas fa-cogs" type="button"></button>
-                <!--====== Menu ======-->
-                <div class="ah-lg-mode">
-                    <span class="ah-close">✕ Close</span>
-                    <!--====== List ======-->
-                    <ul class="ah-list ah-list--design1 ah-list--link-color-secondary">
-                        <li class="has-dropdown" data-tooltip="tooltip" data-placement="left" title="Account">
-                            <a><i class="far fa-user-circle"></i></a>
-                            <!--====== Dropdown ======-->
-                            <span class="js-menu-toggle"></span>
-                            <ul style="width:120px">
-                                @if(Auth::check())
-                                <li>
-                                    <a href="{{ url('user/account')}}"><i class="fas fa-user-circle u-s-m-r-6"></i>
-                                    <span>Account</span></a>
-                                </li>
-                                <li>
-                                    <a href="{{ url('user/logout')}}"><i class="fas fa-lock-open u-s-m-r-6"></i>
-                                    <span>Signout</span></a>
-                                </li>
-                                @else
-                                <li>
-                                    <a href="{{ url('user/register')}}"><i class="fas fa-user-plus u-s-m-r-6"></i>
-                                    <span>Signup</span></a>
-                                </li>
-                                <li>
-                                    <a href="{{ url('user/login')}}"><i class="fas fa-lock u-s-m-r-6"></i>
-                                    <span>Signin</span></a>
-                                </li>
-                                @endif
-                            </ul>
-                            <!--====== End - Dropdown ======-->
-                        </li>
-                        <li data-tooltip="tooltip" data-placement="left" title="Contact">
-                            <a href="tel:+233 244491803"><i class="fas fa-phone-volume"></i></a>
-                        </li>
-                        <li data-tooltip="tooltip" data-placement="left" title="Mail">
-                            <a href="mailto:info@rhodelitconsult.com"><i class="far fa-envelope"></i></a>
-                        </li>
-                    </ul>
-                    <!--====== End - List ======-->
+    <!--====== Nav 1 ======-->
+    <nav class="primary-nav primary-nav-wrapper--border">
+        <div class="container">
+            <!--====== Primary Nav ======-->
+            <div class="primary-nav">
+                <!--====== Main Logo ======-->
+                <a class="main-logo" href="{{ url('/')}}">
+                    <img src="{{ asset('front/images/logo/logo-1.png') }}" alt=""></a>
+                <!--====== End - Main Logo ======-->
+                <!--====== Search Form ======-->
+                <form class="main-form" action="{{ url('search-products') }}" method="get">
+                    <label for="main-search"></label>
+                    <input class="input-text input-text--border-radius input-text--style-1" type="text" id="main-search" name="query" placeholder="Search">
+                    <button class="btn btn--icon fas fa-search main-search-button" type="submit"></button>
+                </form>
+                <!--====== End - Search Form ======-->
+                <!--====== Dropdown Main plugin ======-->
+                <div class="menu-init" id="navigation">
+                    <button class="btn btn--icon toggle-button toggle-button--secondary fas fa-cogs" type="button"></button>
+                    <!--====== Menu ======-->
+                    <div class="ah-lg-mode">
+                        <span class="ah-close">✕ Close</span>
+                        <!--====== List ======-->
+                        <ul class="ah-list ah-list--design1 ah-list--link-color-secondary">
+                            <li class="has-dropdown" data-tooltip="tooltip" data-placement="left" title="Account">
+                                <a><i class="far fa-user-circle"></i></a>
+                                <!--====== Dropdown ======-->
+                                <span class="js-menu-toggle"></span>
+                                <ul style="width:120px">
+                                    @if(Auth::check())
+                                    <li>
+                                        <a href="{{ url('user/account')}}"><i class="fas fa-user-circle u-s-m-r-6"></i>
+                                            <span>Account</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="{{ url('user/logout')}}"><i class="fas fa-lock-open u-s-m-r-6"></i>
+                                            <span>Signout</span></a>
+                                    </li>
+                                    @else
+                                    <li>
+                                        <a href="{{ url('user/register')}}"><i class="fas fa-user-plus u-s-m-r-6"></i>
+                                            <span>Signup</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="{{ url('user/login')}}"><i class="fas fa-lock u-s-m-r-6"></i>
+                                            <span>Signin</span></a>
+                                    </li>
+                                    @endif
+                                </ul>
+                                <!--====== End - Dropdown ======-->
+                            </li>
+                            <li data-tooltip="tooltip" data-placement="left" title="Contact">
+                                <a href="tel:+233 244491803"><i class="fas fa-phone-volume"></i></a>
+                            </li>
+                            <li data-tooltip="tooltip" data-placement="left" title="Mail">
+                                <a href="mailto:info@rhodelitconsult.com"><i class="far fa-envelope"></i></a>
+                            </li>
+                        </ul>
+                        <!--====== End - List ======-->
+                    </div>
+                    <!--====== End - Menu ======-->
                 </div>
-                <!--====== End - Menu ======-->
+                <!--====== End - Dropdown Main plugin ======-->
             </div>
-            <!--====== End - Dropdown Main plugin ======-->
+            <!--====== End - Primary Nav ======-->
         </div>
-        <!--====== End - Primary Nav ======-->
-    </div>
-</nav>
-<!--====== End - Nav 1 ======-->
-<!--====== Nav 2 ======-->
-<nav class="secondary-nav-wrapper">
-    <div class="container">
-        <!--====== Secondary Nav ======-->
-        <div class="secondary-nav">
-            <!--====== Dropdown Main plugin ======-->
-            <?php /* </div><div class="menu-init" id="navigation1">
+    </nav>
+    <!--====== End - Nav 1 ======-->
+    <!--====== Nav 2 ======-->
+    <nav class="secondary-nav-wrapper">
+        <div class="container">
+            <!--====== Secondary Nav ======-->
+            <div class="secondary-nav">
+                <!--====== Dropdown Main plugin ======-->
+                <?php /* </div><div class="menu-init" id="navigation1">
                 <!-- <button class="btn btn--icon toggle-mega-text toggle-button" type="button">M</button> -->
                 <!--====== Menu ======-->
                 <div class="ah-lg-mode">
@@ -757,92 +758,92 @@ $totalCartItems = totalCartItems();
                 </div>
                 <!--====== End - Menu ======-->
             </div>*/ ?>
-            <!--====== End - Dropdown Main plugin ======-->
-            <!--====== Dropdown Main plugin ======-->
-            <div class="menu-init" id="navigation2">
-                <button class="btn btn--icon toggle-button toggle-button--secondary fas fa-cog" type="button"></button>
-                <!--====== Menu ======-->
-                <div class="ah-lg-mode">
-                    <span class="ah-close">✕ Close</span>
-                    <!--====== List ======-->
-                    <ul class="ah-list ah-list--design2 ah-list--link-color-secondary">
-                        <li>
-                            <a href="shop-side-version-2.html">NEW ARRIVALS</a>
-                        </li>
-                @foreach($categories as $category)
-                <li class="has-dropdown">
-                    <a href="#">
-                    {{$category['category_name']}}<i @if(count($category['subcategories'])) class="fas fa-angle-down u-s-m-l-6" @endif></i></a>@if(count($category['subcategories']))
-                    <!--====== Dropdown ======-->
-                    <span class="js-menu-toggle"></span>
-                    <ul style="width:170px">
-                        @foreach($category['subcategories'] as $subcategory)
-                        <li class="has-dropdown has-dropdown--ul-left-100">
-                            <a href="{{ url($subcategory['url']) }}">{{$subcategory['category_name']}}@if(count($subcategory['subcategories']))<i class="fas fa-angle-down i-state-right u-s-m-l-6"></i>@endif</a>
-                            @if(count($subcategory['subcategories']))
-                            <!--====== Dropdown ======-->
-                            <span class="js-menu-toggle"></span>
-                            <ul style="width:118px">
-                                @foreach($subcategory['subcategories'] as $subsubcategory)
-                                <li>
-                                    <a href="{{ url($subsubcategory['url']) }}">{{$subsubcategory['category_name']}}</a>
-                                </li>
-                                @endforeach
-                            </ul>
-                            <!--====== End - Dropdown ======-->
-                            @endif
+                <!--====== End - Dropdown Main plugin ======-->
+                <!--====== Dropdown Main plugin ======-->
+                <div class="menu-init" id="navigation2">
+                    <button class="btn btn--icon toggle-button toggle-button--secondary fas fa-cog" type="button"></button>
+                    <!--====== Menu ======-->
+                    <div class="ah-lg-mode">
+                        <span class="ah-close">✕ Close</span>
+                        <!--====== List ======-->
+                        <ul class="ah-list ah-list--design2 ah-list--link-color-secondary">
+                            <li>
+                                <a href="shop-side-version-2.html">NEW ARRIVALS</a>
                             </li>
-                        @endforeach
-                        </li>
+                            @foreach($categories as $category)
+                            <li class="has-dropdown">
+                                <a href="#">
+                                    {{$category['category_name']}}<i @if(count($category['subcategories'])) class="fas fa-angle-down u-s-m-l-6" @endif></i></a>@if(count($category['subcategories']))
+                                <!--====== Dropdown ======-->
+                                <span class="js-menu-toggle"></span>
+                                <ul style="width:170px">
+                                    @foreach($category['subcategories'] as $subcategory)
+                                    <li class="has-dropdown has-dropdown--ul-left-100">
+                                        <a href="{{ url($subcategory['url']) }}">{{$subcategory['category_name']}}@if(count($subcategory['subcategories']))<i class="fas fa-angle-down i-state-right u-s-m-l-6"></i>@endif</a>
+                                        @if(count($subcategory['subcategories']))
+                                        <!--====== Dropdown ======-->
+                                        <span class="js-menu-toggle"></span>
+                                        <ul style="width:118px">
+                                            @foreach($subcategory['subcategories'] as $subsubcategory)
+                                            <li>
+                                                <a href="{{ url($subsubcategory['url']) }}">{{$subsubcategory['category_name']}}</a>
+                                            </li>
+                                            @endforeach
+                                        </ul>
+                                        <!--====== End - Dropdown ======-->
+                                        @endif
+                                    </li>
+                                    @endforeach
+                            </li>
 
-                    </ul>
-                    <!--====== End - Dropdown ======-->
-                    @endif
-                </li>
-                @endforeach
-                <li>
-                    <a href="{{ url('product/'.$product['id'])}}">FEATURED PRODUCTS</a>
-                </li>
-            </ul>
-            <!--====== End - List ======-->
-        </div>
-        <!--====== End - Menu ======-->
-    </div>
-            <!--====== End - Dropdown Main plugin ======-->
-            <!--====== Dropdown Main plugin ======-->
-            <div class="menu-init" id="navigation3">
-                <button class="btn btn--icon toggle-button toggle-button--secondary fas fa-shopping-bag toggle-button-shop" type="button"></button>
-                <span class="total-item-round totalCartItems">{{$totalCartItems}}</span>
-                <!--====== Menu ======-->
-                <div class="ah-lg-mode">
-                    <span class="ah-close">✕ Close</span>
-                    <!--====== List ======-->
-                    <ul class="ah-list ah-list--design1 ah-list--link-color-secondary">
+                        </ul>
+                        <!--====== End - Dropdown ======-->
+                        @endif
+                        </li>
+                        @endforeach
                         <li>
-                            <a href="index.html"><i class="fas fa-home u-c-brand"></i></a>
+                            <a href="shop-side-version-2.html">FEATURED PRODUCTS</a>
                         </li>
-                        <li>
-                            <a href="{{route("wishlists")}}"><i class="far fa-heart"></i></a>
-                        </li>
-                        <li class="has-dropdown">
-                            <a class="mini-cart-shop-link"><i class="fas fa-shopping-bag"></i>
-                            <span class="total-item-round totalCartItems">{{$totalCartItems}}</span></a>
-                            <!--====== Dropdown ======-->
-                            <span class="js-menu-toggle"></span>
-                            <div class="mini-cart" id="appendMiniCartItems">
-                                @include('front.layout.header_cart_items')
-                            </div>
-                            <!--====== End - Dropdown ======-->
-                        </li>
-                    </ul>
-                    <!--====== End - List ======-->
+                        </ul>
+                        <!--====== End - List ======-->
+                    </div>
+                    <!--====== End - Menu ======-->
                 </div>
-                <!--====== End - Menu ======-->
+                <!--====== End - Dropdown Main plugin ======-->
+                <!--====== Dropdown Main plugin ======-->
+                <div class="menu-init" id="navigation3">
+                    <button class="btn btn--icon toggle-button toggle-button--secondary fas fa-shopping-bag toggle-button-shop" type="button"></button>
+                    <span class="total-item-round totalCartItems">{{$totalCartItems}}</span>
+                    <!--====== Menu ======-->
+                    <div class="ah-lg-mode">
+                        <span class="ah-close">✕ Close</span>
+                        <!--====== List ======-->
+                        <ul class="ah-list ah-list--design1 ah-list--link-color-secondary">
+                            <li>
+                                <a href="index.html"><i class="fas fa-home u-c-brand"></i></a>
+                            </li>
+                            <li>
+                                <a href="{{route("wishlists")}}"><i class="far fa-heart"></i></a>
+                            </li>
+                            <li class="has-dropdown">
+                                <a class="mini-cart-shop-link"><i class="fas fa-shopping-bag"></i>
+                                    <span class="total-item-round totalCartItems">{{$totalCartItems}}</span></a>
+                                <!--====== Dropdown ======-->
+                                <span class="js-menu-toggle"></span>
+                                <div class="mini-cart" id="appendMiniCartItems">
+                                    @include('front.layout.header_cart_items')
+                                </div>
+                                <!--====== End - Dropdown ======-->
+                            </li>
+                        </ul>
+                        <!--====== End - List ======-->
+                    </div>
+                    <!--====== End - Menu ======-->
+                </div>
+                <!--====== End - Dropdown Main plugin ======-->
             </div>
-            <!--====== End - Dropdown Main plugin ======-->
+            <!--====== End - Secondary Nav ======-->
         </div>
-        <!--====== End - Secondary Nav ======-->
-    </div>
-</nav>
-<!--====== End - Nav 2 ======-->
+    </nav>
+    <!--====== End - Nav 2 ======-->
 </header>
